307564daabe205875c46c6f0075a8653c67b74f5
Cha_Cha <dkswndrua1@gmail.com>
2022-11-22 18:51:18 +0900

    signup activity update

--- app/src/main/java/com/example/projectpbl/HomeActivity.kt
+++ app/src/main/java/com/example/projectpbl/HomeActivity.kt
@@ -31,8 +31,6 @@ class HomeActivity : AppCompatActivity(){
             true
         }
 
-
-        binding.success.text= Firebase.auth.currentUser?.uid?:"No Userr"
     }
     private fun setFragment(tag: String, fragment: Fragment){
         val manager: FragmentManager=supportFragmentManager
