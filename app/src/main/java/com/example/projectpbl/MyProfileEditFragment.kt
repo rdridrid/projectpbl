324a7701a4cf638f9eb5de0de6ddd923a35b9925
Cha_Cha <dkswndrua1@gmail.com>
2022-11-25 20:29:09 +0900

    re push

--- app/src/main/java/com/example/projectpbl/MyProfileEditFragment.kt
+++ app/src/main/java/com/example/projectpbl/MyProfileEditFragment.kt
@@ -49,104 +49,96 @@ class MyProfileEditFragment : Fragment() {
         }
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentMyProfileEditBinding.inflate(inflater, container, false)
+        val auth = Firebase.auth
+        val database = Firebase.database
+        val uid = Firebase.auth.currentUser!!.uid
+        val itemsRef = database.getReference("Users").child(uid)
+        val UserName = binding.editMyprofileUsername
+        val UserStatusMessage = binding.editMyprofileStatusMessage
+        val photo = binding.editMyprofileImage
+        val edit = binding.editMyprofileImageText
+
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == AppCompatActivity.RESULT_OK) {
+
+                    imageUri = result.data?.data //이미지 경로 원본
+                    photo.setImageURI(imageUri)
+
+                    /*
+                    //기존 사진을 삭제 후 새로운 사진을 등록
+                    fireStorage.child("userImages/$uid/photo").delete().addOnSuccessListener {
+                        fireStorage.child("userImages/$uid/photo").putFile(imageUri!!).addOnSuccessListener {
+                            fireStorage.child("userImages/$uid/photo").downloadUrl.addOnSuccessListener {
+                                val photoUri : Uri = it
+                                println("$photoUri")
+                                fireDatabase.child("users/$uid/profileImageUrl").setValue(photoUri.toString())
+                                Toast.makeText(requireContext(), "프로필사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    } */
+
+                    Log.d("이미지", "성공")
+                } else {
+                    Log.d("이미지", "실패")
+                }
+            }
+
+        edit.setOnClickListener {
+            val intentImage = Intent(Intent.ACTION_PICK)
+            intentImage.type = MediaStore.Images.Media.CONTENT_TYPE
+            getContent.launch(intentImage)
+        }
+
+
+        itemsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (child in dataSnapshot.children) {
+                    if (child.key == "userName") {
+                        val test = child.value.toString()
+                        UserName.setText(test)
+                    }
+                    if (child.key == "UserStatusMessage") {
+                        val temp = child.value.toString()
+                        UserStatusMessage.setText(temp)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("loadItem:onCancelled")
+            }
+        })
+        binding.editMyprofileCheck.setOnClickListener {
+            val tempprofileusername = UserName.text.toString()
+            val tempprofileuserstatusmessage = UserStatusMessage.text.toString()
+            if (tempprofileusername.isEmpty()) {
+                println("이름이 없음")
+            } else { //프로필 변경 성공
+                database.getReference("Users").child(uid!!).child("userName")
+                    .setValue(tempprofileusername)
+                database.getReference("Users").child(uid!!).child("UserStatusMessage")
+                    .setValue(tempprofileuserstatusmessage)
+            }
+            (activity as HomeActivity) changeFragmentWithBackStack (MyProfileFragment.newInstance())
+        }
+        return binding.root
     }
 
+    companion object {
+        private var imageUri: Uri? = null
+        private val fireStorage = FirebaseStorage.getInstance().reference
+        private val fireDatabase = FirebaseDatabase.getInstance().reference
+        private val user = Firebase.auth.currentUser
+        private val uid = user?.uid.toString()
 
-
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            val binding = FragmentMyProfileEditBinding.inflate(inflater, container, false)
-            val auth = Firebase.auth
-            val database = Firebase.database
-            val uid = Firebase.auth.currentUser!!.uid
-            val itemsRef = database.getReference("Users").child(uid)
-            val UserName = binding.editMyprofileUsername
-            val UserStatusMessage = binding.editMyprofileStatusMessage
-            val photo = binding.editMyprofileImage
-            val edit = binding.editMyprofileImageText
-
-            val getContent =
-                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-                    if(result.resultCode == AppCompatActivity.RESULT_OK) {
-
-                        imageUri = result.data?.data //이미지 경로 원본
-                        photo.setImageURI(imageUri)
-
-                        /*
-                        //기존 사진을 삭제 후 새로운 사진을 등록
-                        fireStorage.child("userImages/$uid/photo").delete().addOnSuccessListener {
-                            fireStorage.child("userImages/$uid/photo").putFile(imageUri!!).addOnSuccessListener {
-                                fireStorage.child("userImages/$uid/photo").downloadUrl.addOnSuccessListener {
-                                    val photoUri : Uri = it
-                                    println("$photoUri")
-                                    fireDatabase.child("users/$uid/profileImageUrl").setValue(photoUri.toString())
-                                    Toast.makeText(requireContext(), "프로필사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                        } */
-                        Log.d("이미지", "성공")
-                    }
-                    else{
-                        Log.d("이미지", "실패")
-                    }
-                }
-
-            edit.setOnClickListener {
-                val intentImage = Intent(Intent.ACTION_PICK)
-                intentImage.type = MediaStore.Images.Media.CONTENT_TYPE
-                getContent.launch(intentImage)
-            }
-
-
-            itemsRef.addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    for (child in dataSnapshot.children) {
-                        if (child.key == "userName") {
-                            val test = child.value.toString()
-                            UserName.setText(test)
-                        }
-                        if (child.key == "UserStatusMessage") {
-                            val temp = child.value.toString()
-                            UserStatusMessage.setText(temp)
-                        }
-                    }
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    println("loadItem:onCancelled")
-                }
-            })
-            binding.editMyprofileCheck.setOnClickListener {
-                val tempprofileusername = UserName.text.toString()
-                val tempprofileuserstatusmessage = UserStatusMessage.text.toString()
-                if (tempprofileusername.isEmpty()) {
-                    println("이름이 없음")
-                } else { //프로필 변경 성공
-                    database.getReference("Users").child(uid!!).child("userName")
-                        .setValue(tempprofileusername)
-                    database.getReference("Users").child(uid!!).child("UserStatusMessage")
-                        .setValue(tempprofileuserstatusmessage)
-                }
-                (activity as HomeActivity) changeFragmentWithBackStack (MyProfileFragment.newInstance())
-            }
-            return binding.root
+        fun newInstance(): MyProfileEditFragment {
+            return MyProfileEditFragment()
         }
-
-
-
-        companion object {
-            private var imageUri : Uri? = null
-            private val fireStorage = FirebaseStorage.getInstance().reference
-            private val fireDatabase = FirebaseDatabase.getInstance().reference
-            private val user = Firebase.auth.currentUser
-            private val uid = user?.uid.toString()
-
-            fun newInstance() : MyProfileEditFragment {
-                return MyProfileEditFragment()
-            }
-        }
+    }
 }
